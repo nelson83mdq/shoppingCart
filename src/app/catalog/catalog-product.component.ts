366abc7cbda0acc9f505a79f19e63568a6264696
Nelson Damian Brizuela <nelson83mdq@gmail.com>
2023-08-17 18:31:24 -0300

    *

--- src/app/catalog/catalog-product.component.ts
+++ src/app/catalog/catalog-product.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Product } from './product';
+import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
+import { mapProductToCartItem } from '../helpers/map-product-to-cart-items-helper';
 
 @Component({
   selector: 'app-catalog-product',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog-product.component.css']
 })
 export class CatalogProductComponent {
+  @Input() product! : Product;
 
+  constructor( private scService: ShoppingCartService) {}
+
+  addToCart() : void{
+    const cartItem = mapProductToCartItem(this.product);
+    this.scService.addItem(cartItem);
+  }
 }
